0430646c13e13e7d309e7396ba13854962b28a7c
gregorio pirolini <gregorioxx77@yahoo.de>
2023-10-09 20:03:49 +0200

    back to school at 8

---
+++ bootstrap/bootstrap.md
@@ -0,0 +1,10 @@
+form-label
+form-text
+form-control
+
+novalidate
+
+autocomplete="given-name"
+
+   <div class="valid-feedback">looks good!</div>
+          <div class="invalid-feedback">please enter your first name</div>
--- bootstrap/script2.js
+++ bootstrap/script2.js
@@ -1,16 +1,16 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
+(function () {
   "use strict";
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
+  var forms = document.querySelectorAll(".needs-validation");
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
+  Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener(
       "submit",
-      (event) => {
-        if (!form.checkValidity()) {
+      function (event) {
+        if (form.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
         }
